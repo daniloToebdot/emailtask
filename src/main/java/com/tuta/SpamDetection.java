49d43ce311f78c1937df06b2acf32f3be5381caa
D.Souza <toebdot@gmail.com>
2024-11-13 20:18:17 +0100

    minor refactor

--- src/main/java/com/tuta/SpamDetection.java
+++ src/main/java/com/tuta/SpamDetection.java
@@ -7,30 +7,33 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+
 public class SpamDetection {
 
     private static final double CUTOFF_DISTANCE = 0.2;
 
-
     public static List<Email> verifyEmailsForSpam(List<Email> emails) {
         Map<String, Email> verifiedEmails = new HashMap<>();
         for (int i = 0; i < emails.size(); i++) {
-            Email first = emails.get(i);
-            if(verifiedEmails.containsKey(first.getTitle())){
-                continue;
-                }
-                boolean firstDetectedAsSpam = false;
-                for (int j = i + 1; j < emails.size(); j++) {
-                    Email second = emails.get(j);
-                    double distance = getNormalizedLevenshteinDistance(emails.get(i), emails.get(j));
-                    if(distance <= CUTOFF_DISTANCE){
-                        firstDetectedAsSpam = true;
+            Email current = emails.get(i);
 
-                        verifiedEmails.put(second.getTitle(), new Email(second, true));
+            boolean isEmailAlreadyVerified = verifiedEmails.containsKey(current.getTitle());
+            if(isEmailAlreadyVerified){
+                continue;
+            }
+
+            boolean firstDetectedAsSpam = false;
+            for (int j = i + 1; j < emails.size(); j++) {
+                Email comparisonEmail = emails.get(j);
+                double distance = getNormalizedLevenshteinDistance(current, comparisonEmail);
+
+                if(distance <= CUTOFF_DISTANCE){
+                    firstDetectedAsSpam = true;
+                    verifiedEmails.put(comparisonEmail.getTitle(), new Email(comparisonEmail, true));
                 }
             }
 
-            verifiedEmails.put(first.getTitle(), new Email(first, firstDetectedAsSpam));
+            verifiedEmails.put(current.getTitle(), new Email(current, firstDetectedAsSpam));
         }
 
         return new ArrayList<>(verifiedEmails.values());
